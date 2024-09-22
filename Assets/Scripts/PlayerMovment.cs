58c6cdce903197c998cce1f364abebc458842490
AntoniLawa <157843604+AntoniLawa@users.noreply.github.com>
2024-09-22 19:24:47 +0200

    edit terrain make new Script to movement

--- Assets/Scripts/PlayerMovment.cs
+++ Assets/Scripts/PlayerMovment.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Moving : MonoBehaviour
+
+public class NewBehaviourScript : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    [Header("Movement")]
+
     void Start()
     {
         
